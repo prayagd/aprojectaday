82cbca799a26d3db04f4774c5af423af39af1567
Prayag Desale <prayagdesale@Prayags-MacBook-Pro.local>
2021-05-08 19:45:57 +0530

    second project ticker search

---
+++ ticker_search/main.js
@@ -0,0 +1,15 @@
+let ticker = document.getElementById("ticker")
+ticker.addEventListener("change", function(val){
+    let tickerVal = val.target.value.toUpperCase()
+    val.target.value = ""
+    console.log(tickerVal)
+    axios.get("https://api.twelvedata.com/stocks", {
+        params: {
+            symbol: tickerVal,
+        }
+    })
+         .then(function(response){
+             let stockName = response.data.data[0].name
+             document.getElementById("stock").textContent = stockName
+         })
+})
