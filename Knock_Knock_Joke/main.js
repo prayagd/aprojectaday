8d2121dfd4a3f29768b52756f662f87dbd68a3d0
Prayag Desale <prayagdesale@Prayags-MacBook-Pro.local>
2021-05-11 19:18:55 +0530

    fourth project complete

--- Knock_Knock_Joke/main.js
+++ Knock_Knock_Joke/main.js
@@ -1,23 +1,48 @@
 
+let value = []
 
+function reset(){
+    fetch("https://raw.githubusercontent.com/prayagd/Knock_knock_jokes/main/joke.json")
+    .then(res => res.json())
+    .then(function(res){
+        value = res.data[Math.floor(Math.random() * 100)]
+    })
+    .then(() => console.log(value))
+    setup.textContent = "Knock Knock"
+    generator.textContent = "Who's There?"
+}
 
 let setup = document.getElementById("setup")
 let generator = document.getElementById("generator")
 
 
-generator.addEventListener("click", firstPart)
 
+let onClick = firstPart()
 
 function firstPart(){
-    fetch("https://raw.githubusercontent.com/prayagd/Knock_knock_jokes/main/joke.json")
-    .then(res => res.json())
-    .then(function(res){
-        let jokeString = res.data[Math.floor(Math.random() * 101)]
-        let jokeArray = jokeString.split("\n")
-        setup.textContent = jokeArray[2]
-        generator.textContent = jokeArray[3]
-    })
+    let counter = 0;
+    return function(){
+        let jokeArray = value.split("\n")
+        if(counter == 0){
+            setup.textContent = jokeArray[2]
+            generator.textContent = jokeArray[3]
+           counter++
+        }
+        else if(counter == 1){
+            setup.textContent = jokeArray[4]
+            generator.textContent = "HaHaHaHaHaHaHaHa"
+            counter++
+        }
+        else{
+            counter = 0
+            reset()
+        }
+
+    }
 }
+
+generator.addEventListener("click", onClick)
+reset()
 
 
 
