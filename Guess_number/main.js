afae0ef5795918075d17739ab68572fb7abeb062
Prayag Desale <prayagdesale@Prayags-MacBook-Pro.local>
2021-05-17 17:58:09 +0530

    Guess number styling complete

--- Guess_number/main.js
+++ Guess_number/main.js
@@ -1,31 +1,33 @@
 let start = document.getElementById("start")
 let guess = document.getElementById("guess")
 let again = document.getElementById("again")
+let desc = document.getElementById("desc")
 
 
 let onChange = guestimate()
 
 function guestimate(){
-    let counter = 4
+    let counter = 5
     let randomNumber = Math.floor(Math.random() * 100);
     console.log(randomNumber)
     return function(){
-        if(counter != 0){
+        if(counter > 0){
+            console.log(counter)
             if(guess.value == randomNumber){
-                console.log("You Win")
+                desc.textContent = "You Win 🥳"
                 again.hidden = false
             }
             else if(guess.value < randomNumber){
-                console.log(`Your number is greater than ${guess.value}`)
+                desc.textContent = `Your number is greater than ${guess.value}. You have ${counter} attempts`
                 counter--
             }
             else if(guess.value > randomNumber){
-                console.log(`Your number is less than ${guess.value}`)
+                desc.textContent = `Your number is less than ${guess.value}. You have ${counter} attempts`
                 counter--
             }
         }
         else{
-            console.log(`You Lose. The number is ${guess.value}`)
+            desc.textContent = `You Lose 😢. The number is ${guess.value}`
             again.hidden = false
         }
         guess.value = ""
