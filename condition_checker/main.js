5c8c1e43062aaf57b0ac3ac05e7d417120e02028
Prayag <prayagd21@gmail.com>
2021-05-22 18:45:33 +0530

    15th project complete

--- condition_checker/main.js
+++ condition_checker/main.js
@@ -2,25 +2,29 @@ let ops = document.getElementById("ops")
 let input1 = document.getElementById("input1")
 let input2 = document.getElementById("input2")
 let check = document.getElementById("check")
+let result = document.getElementById("result")
 
-function compareString(){
-    console.log(input2.value)
-    console.log(input1.value)
-    if(input1.value.length == 0 || input2.value.length == 0 ){
-        console.log("One field incomplete")
-    }
-}
 
-input1.addEventListener("change", function(val){
-    compareString()
-})
-
-input2.addEventListener("change", function(val){
-    compareString()
-})
 
 check.addEventListener("click", function(val){
-    compareString()
+    if(input1.value.length == 0 || input2.value.length == 0 ){
+        result.textContent = "Fields incomplete"
+    }
+    else{
+        if(ops.value == "=="){
+            result.textContent = input1.value == input2.value
+        }
+        else if(ops.value == "==="){
+            result.textContent = input1.value === input2.value
+        }
+        else if(ops.value == "!="){
+            result.textContent = input1.value != input2.value
+        }
+        else if(ops.value == "!=="){
+            result.textContent = input1.value !== input2.value
+        }
+        
+    }
 })
 
 
