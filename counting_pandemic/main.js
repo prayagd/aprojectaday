1a552a9a4b1cdee39f1c9877d6f436cecac22a1c
Prayag <prayagd21@gmail.com>
2021-05-23 20:12:04 +0530

    17th project styling needed

---
+++ counting_pandemic/main.js
@@ -0,0 +1,7 @@
+let count = document.getElementById("count")
+let date = new Date()
+let date1 = new Date("December 1, 2020")
+let value = date - date1
+let current = new Date(value)
+
+console.log(current)
