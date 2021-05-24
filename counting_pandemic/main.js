28257f158651056f1cda6aa773522eb76afca617
Prayag <prayagd21@gmail.com>
2021-05-24 15:35:43 +0530

    17th project complete

--- counting_pandemic/main.js
+++ counting_pandemic/main.js
@@ -1,7 +1,17 @@
 let count = document.getElementById("count")
 let date = new Date()
-let date1 = new Date("December 1, 2020")
+let date1 = new Date("December 1, 2019")
 let value = date - date1
-let current = new Date(value)
 
-console.log(current)
+let day = 1000 * 60 * 60 * 24
+let days = Math.floor(value/day)
+let months = Math.floor(days/31)
+let years = 0
+
+if(months > 12){
+    years++
+    months = months - 12
+}
+ 
+count.textContent = `${years} year and ${months} months`
+
