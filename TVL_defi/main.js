6f729fd8e14279ae257d58aa2b0afd7342136034
Prayag <prayagd21@gmail.com>
2021-06-05 21:45:39 +0530

    29th project final style needed

---
+++ TVL_defi/main.js
@@ -0,0 +1,9 @@
+let heart1 = document.getElementById("heart1")
+let heart2 = document.getElementById("heart2")
+
+heart1.addEventListener("click", function(){
+    heart2.hidden = false
+    heart1.hidden = true
+})
+
+
